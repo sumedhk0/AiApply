64b4cfa2e1b223c0c6a3ce71568ce778f36bd139
Sumedh K <sumedh-k@outlook.com>
2026-02-16 18:44:22 -0500

    render stuff

--- build.sh
+++ build.sh
@@ -3,19 +3,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Create directories for file uploads (on persistent disk if available)
+# Create directories for file uploads
 mkdir -p user_resumes
 mkdir -p user_transcripts
 mkdir -p uploads
 mkdir -p generated_cover_letters
-
-# If persistent disk is mounted, create symlinks
-if [ -d "/opt/render/project/src/user_data" ]; then
-    mkdir -p /opt/render/project/src/user_data/user_resumes
-    mkdir -p /opt/render/project/src/user_data/user_transcripts
-
-    # Replace local dirs with symlinks to persistent disk
-    rm -rf user_resumes user_transcripts
-    ln -sf /opt/render/project/src/user_data/user_resumes user_resumes
-    ln -sf /opt/render/project/src/user_data/user_transcripts user_transcripts
-fi
